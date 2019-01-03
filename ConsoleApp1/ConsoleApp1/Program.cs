9a0fce30d173207f39048030a94763c73bec9364
Donna Davis <ddavis@bstglobal.com>
2019-01-03 14:37:27 -0500

    my new feature changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleApp1
             // comment was edited in github
             // code was added in vs
             // more code added in vs
+
+            // Code to call feature1
         }
     }
 }
